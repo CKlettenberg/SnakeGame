48f1755ed8aee4f6ad3865d0959cf72215a34c72
kasutaja <klettenberg@gmail.com>
2025-02-11 10:59:08 +0200

    snake

---
+++ src/SnakeGame.java
@@ -0,0 +1,165 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class SnakeGame extends JPanel implements ActionListener {
+    private final int TILE_SIZE = 20;
+    private final int WIDTH = 400;
+    private final int HEIGHT = 400;
+    private final LinkedList<Point> snake = new LinkedList<>();
+    private Point food;
+    private Color foodColor;
+    private char direction = 'R';
+    private boolean running = true;
+    private final Timer timer;
+
+    public SnakeGame() {
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setBackground(Color.BLACK);
+        setFocusable(true);
+        initGame();
+
+
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_UP:
+                        if (direction != 'D') direction = 'U';
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        if (direction != 'U') direction = 'D';
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        if (direction != 'R') direction = 'L';
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        if (direction != 'L') direction = 'R';
+                        break;
+                    case KeyEvent.VK_R:
+                        if (!running) {
+                            initGame();
+                            running = true;
+                            timer.start();
+                        }
+                        break;
+                }
+            }
+        });
+
+        timer = new Timer(100, this);
+        timer.start();
+    }
+
+    private void initGame() {
+        snake.clear();
+        snake.add(new Point(5, 5));
+        direction = 'R';
+        spawnFood();
+        running = true;
+    }
+
+    private void spawnFood() {
+        Random rand = new Random();
+        food = new Point(rand.nextInt(WIDTH / TILE_SIZE), rand.nextInt(HEIGHT / TILE_SIZE));
+        foodColor = getRandomColor();
+        while (snake.contains(food)) {
+            food = new Point(rand.nextInt(WIDTH / TILE_SIZE), rand.nextInt(HEIGHT / TILE_SIZE));
+            foodColor = getRandomColor();
+        }
+    }
+
+    private Color getRandomColor() {
+        Random rand = new Random();
+        return new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256)); // RGB values between 0-255
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (running) {
+            drawSnake(g);
+            drawFood(g);
+        } else {
+            gameOver(g);
+        }
+    }
+
+    private void drawSnake(Graphics g) {
+        g.setColor(Color.GREEN);
+        for (Point point : snake) {
+            g.fillRect(point.x * TILE_SIZE, point.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+        }
+    }
+
+    private void drawFood(Graphics g) {
+        g.setColor(foodColor);
+        g.fillRect(food.x * TILE_SIZE, food.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    }
+
+    private void gameOver(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.BOLD, 30));
+        g.drawString("Game Over!", WIDTH / 4, HEIGHT / 2);
+        g.setFont(new Font("Arial", Font.PLAIN, 20));
+        g.drawString("Press 'R' to Restart", WIDTH / 6, HEIGHT / 2 + 30);
+    }
+
+    private void move() {
+        Point head = snake.getFirst();
+        Point newHead = new Point(head.x, head.y);
+
+        switch (direction) {
+            case 'U':
+                newHead.y--;
+                break;
+            case 'D':
+                newHead.y++;
+                break;
+            case 'L':
+                newHead.x--;
+                break;
+            case 'R':
+                newHead.x++;
+                break;
+        }
+
+
+        if (newHead.x < 0 || newHead.x >= WIDTH / TILE_SIZE ||
+                newHead.y < 0 || newHead.y >= HEIGHT / TILE_SIZE ||
+                snake.contains(newHead)) {
+            running = false;
+            timer.stop();
+        } else {
+            snake.addFirst(newHead);
+            if (newHead.equals(food)) {
+                spawnFood();
+            } else {
+                snake.removeLast();
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (running) {
+            move();
+            repaint();
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("USSIPESA");
+        SnakeGame game = new SnakeGame();
+        frame.add(game);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setResizable(false);
+    }
+}
